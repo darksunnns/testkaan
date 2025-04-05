e5a5c0d3d1ead99c2ac01e715e98af6475a77734
kaan <darksunnn-kaan@hotmail.com>
2025-04-05 03:41:38 +0200

    Update server.ts

--- strapi/config/server.ts
+++ strapi/config/server.ts
@@ -1,8 +1,10 @@
-// Path: ./config/env/production/server.js`
 export default ({ env }) => ({
-  proxy: true,
-  url: env("MY_HEROKU_URL"), // Sets the public URL of the application.
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
+  },
+  webhooks: {
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
