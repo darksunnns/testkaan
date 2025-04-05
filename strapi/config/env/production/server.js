2f8dbe5fec8176c8b3d2dca1b9af3705d77164a3
kaan <darksunnn-kaan@hotmail.com>
2025-04-05 03:49:09 +0200

    Update server.js

--- strapi/config/env/production/server.js
+++ strapi/config/env/production/server.js
@@ -1,7 +1,7 @@
 // Path: ./config/env/production/server.js`
 export default ({ env }) => ({
   proxy: true,
-  url: env('MY_HEROKU_URL'), // Sets the public URL of the application.
+  url: env('https://etkinlik-3f13142e5aab.herokuapp.com/'), // Sets the public URL of the application.
   app: { 
        keys: env.array('APP_KEYS')
      },
